3d703b03a1392f36d7aa7ec37a9700bd7eab4bc2
Harinie Logan <harinielogan@gmail.com>
2026-06-21 11:59:45 +0530

    Added AreaOfCircle.java

---
+++ AreaOfCircle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class AreaOfCircle{
+	public static void main (String[] args){
+		Scanner scan = new Scanner (System.in);
+	    System.out.print("Enter a value of Radius: ");
+	    double num2 = scan.nextInt();
+	    double area = 3.14 * num2 * num2;
+	    System.out.print("The Area of Circle containing values of radius " + num2 + " is " +area );
+	  }
+	}
