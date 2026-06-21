6b2babdacee5ace3a942566b5bc525b8c354062d
Harinie Logan <harinielogan@gmail.com>
2026-06-21 11:46:28 +0530

    Added MultiplyNumbers.java

---
+++ MultiplyNumbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class MultiplyNumbers{
+	public static void main (String[] args){
+		Scanner scan = new Scanner (System.in);
+	    System.out.print("Enter a value for A: ");
+	    int num1 = scan.nextInt();
+	    System.out.print("Enter a value for B: ");
+	    int num2 = scan.nextInt();
+	    int result = num1 * num2;
+	    System.out.print("The Multiplication of " + num1 + " and " + num2 + " is " + result );
+	  }
+	}
