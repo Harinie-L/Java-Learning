e751ee326cf82ebe34f36c25e06e50dad4c4a316
Harinie Logan <harinielogan@gmail.com>
2026-06-21 11:48:13 +0530

    Added DivideNumbers.java

---
+++ DivideNumbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class DivideNumbers{
+	public static void main (String[] args){
+		Scanner scan = new Scanner (System.in);
+	    System.out.print("Enter a value for A: ");
+	    int num1 = scan.nextInt();
+	    System.out.print("Enter a value for B: ");
+	    int num2 = scan.nextInt();
+	    int result = num1 / num2;
+	    System.out.print("The Division of " + num1 + " and " + num2 + " is " + result );
+	  }
+	}
