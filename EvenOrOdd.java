4b11af8859241ae6fd3e36633a85f817142d311e
Harinie Logan <harinielogan@gmail.com>
2026-06-21 12:17:40 +0530

    Added EvenOrOdd.java

---
+++ EvenOrOdd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class EvenOrOdd{
+	public static void main (String[] args){
+		Scanner scan = new Scanner (System.in);
+	    System.out.print("Enter a number to check if it is Even or Odd: ");
+	    int num1 = scan.nextInt();
+	    if(num1 % 2 == 0) {
+	    	System.out.print("You have entered an Even Number");
+	    }
+	    else {
+	    	System.out.print("You have entered an Odd Number");
+	    }
+	}
+}
+
+	   
