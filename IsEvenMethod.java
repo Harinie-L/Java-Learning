5a97fe6f2dde5cf6e7c4fa9169dde7520f853246
Harinie Logan <harinielogan@gmail.com>
2026-06-25 21:36:00 +0530

    Added IsEvenMethod.java

---
+++ IsEvenMethod.java
@@ -0,0 +1,13 @@
+public class IsEvenMethod {
+	public static boolean IsEven (int n) {
+		if(n % 2 ==0 ) {
+            return true;
+        }
+        else {
+            return false;
+        }
+		}
+	public static void main(String[] args) {
+		 System.out.print( IsEven(22));
+		 }
+	}
