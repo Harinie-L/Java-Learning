68a6e4faeedbb2bebd757895a29ab2d28af5bf93
Harinie Logan <harinielogan@gmail.com>
2026-06-25 20:32:35 +0530

    Added ListExample.java

---
+++ ListExample.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.ArrayList;
+public class ListExample {
+	public static void main(String[] args) {
+			List<String> name = new ArrayList<>();
+			name.add("Harinie");
+			name.add("Advik");
+			name.add("Varsana");
+			System.out.print(name);
+			
+		}
+
+	}
