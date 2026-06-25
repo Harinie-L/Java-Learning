244c1df9510db850d7c231f167bece1a96be6dee
Harinie Logan <harinielogan@gmail.com>
2026-06-25 21:40:27 +0530

    Added StringReturn.java

---
+++ StringReturn.java
@@ -0,0 +1,9 @@
+public class StringReturn {
+	public static String getName (String name) {
+		return name;
+		}
+	public static void main(String[] args) {
+		 String result = getName("Harinie");
+		 System.out.print(result);
+		 }
+	}
