514af4a0c4179a920b81915389f2ad2f450a3717
Harinie Logan <harinielogan@gmail.com>
2026-06-26 19:47:38 +0530

    Added StringCompareTo.java

---
+++ StringCompareTo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class StringCompareTo {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter a First String: ");
+		String s1 = scan.nextLine();
+		System.out.print("Enter a Second String: ");
+		String s2 = scan.nextLine();
+		int result = s1.compareTo(s2);
+		System.out.print(result);
+		scan.close();
+		 }
+	}
