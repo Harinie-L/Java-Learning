e9e988f1f3c7e09ca8d848fca00658e0e5a6eee7
Harinie Logan <harinielogan@gmail.com>
2026-06-26 08:47:09 +0530

    Added StringStartEndIndex.java

---
+++ StringStartEndIndex.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class StringStartEndIndex {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter a String: ");
+		String s1 = scan.nextLine();
+		System.out.print("Starting Index: ");
+		int startindex = scan.nextInt();
+		System.out.print("Ending Index: ");
+		int endindex = scan.nextInt();
+		System.out.println("Substring: " +s1.substring(startindex,endindex));
+		
+		scan.close();
+		 }
+	}
