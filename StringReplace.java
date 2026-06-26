4ca1ab39f02412e7ff8aff60a980a8982b5f138e
Harinie Logan <harinielogan@gmail.com>
2026-06-26 08:59:15 +0530

    Added StringReplace.java

---
+++ StringReplace.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class StringReplace {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter a String: ");
+		String s1 = scan.nextLine();
+		System.out.print("Enter a Word to Replace: ");
+		String rep = scan.nextLine();
+		System.out.print("New Word: ");
+		String word = scan.nextLine();
+		System.out.println("Updated String: " +s1.replace(rep, word));
+		scan.close();
+		 }
+	}
