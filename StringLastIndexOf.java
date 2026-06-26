30bcf4dcf35fc0b740e59b298a6c547e9c140447
Harinie Logan <harinielogan@gmail.com>
2026-06-26 09:26:28 +0530

    Added StringLastIndexOf.java

---
+++ StringLastIndexOf.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class StringLastIndexOf {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter a String: ");
+		String s1 = scan.nextLine();
+		System.out.print("Enter a Charecter: ");
+		String c = scan.nextLine();
+		System.out.println("Last Occurence: " +s1.lastIndexOf(c));
+		scan.close();
+		 }
+	}
