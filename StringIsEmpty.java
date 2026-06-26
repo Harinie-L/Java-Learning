bf112e42d7b8ebc962cdb564982aacfb49d2bd00
Harinie Logan <harinielogan@gmail.com>
2026-06-26 09:37:32 +0530

    Added StringIsEmpty.java

---
+++ StringIsEmpty.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class StringIsEmpty {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter a String: ");
+		String s1 = scan.nextLine();
+		System.out.println("Is Empty: " +s1.isEmpty());
+		scan.close();
+		 }
+	}
