ab2fce0b67e4d7fcc32c0aac2c18176db6475ce0
Harinie Logan <harinielogan@gmail.com>
2026-06-26 20:46:15 +0530

    Added ArrayAverage.java

---
+++ ArrayAverage.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ArrayAverage{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int[] marks = new int[5];
+		int sum = 0;
+		double average = 0;
+		System.out.print("Enter your marks: ");
+		for(int i = 0; i<marks.length;i++) {
+		marks[i] = scan.nextInt();
+		}
+		for(int i = 0; i<marks.length;i++) {
+			sum = sum+marks[i];
+			}
+		average = sum/marks.length;
+		System.out.println("Summation of your Marks: " +sum);
+		System.out.println("Average of your Marks: " +average);
+		
+	}
+}
