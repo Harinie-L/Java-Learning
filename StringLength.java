27ce678329b49c7544399a5e4647026981151f85
Harinie Logan <harinielogan@gmail.com>
2026-06-26 08:04:23 +0530

    Added StringLength.java

---
+++ StringLength.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class StringLength {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		System.out.print("Enter your Name: ");
+		 String name = scan.nextLine();
+		 System.out.print(name.length());
+		 }
+	}
