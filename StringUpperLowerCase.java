85a09f4c1980c63247df5c50e268dd81de02f855
Harinie Logan <harinielogan@gmail.com>
2026-06-26 08:31:34 +0530

    Added StringUpperLowerCase.java

---
+++ StringUpperLowerCase.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class StringUpperLowerCase {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter a String: ");
+		String s1 = scan.nextLine();
+		System.out.println("Original String: " +s1);
+		System.out.println("Upper Case: " +s1.toUpperCase());
+		System.out.println("Lower Case: " +s1.toLowerCase());
+		scan.close();
+		 }
+	}
